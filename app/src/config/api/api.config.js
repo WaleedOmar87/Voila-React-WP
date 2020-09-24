3a561b99a7dbf4fcc0fb1c6b7100de42e9809557
waleed-omar <waleed@waleeds-MacBook-Pro.local>
2020-09-24 14:40:43 +0200

    updated blog components and endpoints config

---
+++ app/src/config/api/api.config.js
@@ -0,0 +1,8 @@
+const server = "http://localhost:8888/html/voila-react/wp/wp-json";
+
+export const apiConfig = {
+	menusEndpoint: `${server}/voila-rest/v2/menus`,
+	postsEndpoint: `${server}/wp/v2/posts`,
+	mediaEndpoint: `${server}/wp/v2/media`,
+	widgetsEndpoint: `${server}/`,
+};
